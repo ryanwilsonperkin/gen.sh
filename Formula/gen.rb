b5673bea88ed69c63a7beecbae77708eba01d1e8
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2026-03-13 14:28:01 -0400

    build: update formula sha256 for v0.3.0

--- Formula/gen.rb
+++ Formula/gen.rb
@@ -2,7 +2,7 @@ class Gen < Formula
   desc "Generate shell commands using an LLM"
   homepage "https://github.com/ryanwilsonperkin/gen"
   url "https://github.com/ryanwilsonperkin/gen/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "b5b5ddea358a5b6f7efe7797065b5739cfff227a849eb6b82d13ba792280e51f"
+  sha256 "33bdf8dff4d578b0228d7054a47c68a67ad9495363ae5a273da4f16ff7b95445"
   license "MIT"
 
   depends_on "node"
